9ac41b93422037d66fc85f99d9fb600a96fc3cd0
Aniruddha-972001 <56734645+Aniruddha-972001@users.noreply.github.com>
2021-10-11 15:52:47 +0530

    commit

--- Bank_App/script.js
+++ Bank_App/script.js
@@ -196,13 +196,3 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 });
-
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
